8f6b8886009232ddc20de6fcdf140d81f6cfcf10
Timesemd <sktimesmed@gmail.com>
2022-11-10 08:56:14 +0530

    login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -11,15 +11,56 @@ class Login extends StatelessWidget {
 
 
     return Scaffold(
+      backgroundColor: Colors.indigo,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text("Login",style: TextStyle(fontSize:width/9 ),),
-          Text("Please sign in to continue",style: TextStyle(fontSize:width/15),),
-          TextFormField(),
-          TextFormField(),
-          TextButton(onPressed: (){}, child: Text("login"),)
+          Text("Login",style: TextStyle(fontSize:width/12,fontWeight: FontWeight.bold),),
+          Text("Please sign in to continue.",style: TextStyle(fontSize:width/20,fontWeight: FontWeight.bold),),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.indigo,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide.none,
+                  ),
+
+                  hintStyle: TextStyle(color: Colors.grey,fontFamily: "WorkSansLight"),
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Username'),
+            ),
+          ),
+
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.key,
+                    color: Colors.indigo,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide.none,
+                  ),
+
+                  hintStyle: TextStyle(color: Colors.grey,fontFamily: "WorkSansLight"),
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Password'),
+            ),
+          ),
+
+
+          TextButton(onPressed: (){}, child: Text("Forget Password",style: TextStyle(color: Colors.white),),)
 
 
 
